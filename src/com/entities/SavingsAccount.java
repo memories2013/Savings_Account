44446f76aed734f1babb5431eb9054b8eeb28336
memories2013 <nshivaprasad41@gmail.com>
2021-12-22 18:56:25 +0530

    ADD method to caculate the monthly interest

--- src/com/entities/SavingsAccount.java
+++ src/com/entities/SavingsAccount.java
@@ -4,12 +4,14 @@ public class SavingsAccount {
 
 	private static double annualInterestRate = 0d;
 	private double savingsBalance;
-	
+
 	public SavingsAccount(double savingsBalance) {
 		super();
 		this.savingsBalance = savingsBalance;
 	}
-	
-	
-	
+
+	public void calculateMonthlyInterest() {
+		savingsBalance *= (annualInterestRate / 100) / 12;
+	}
+
 }
