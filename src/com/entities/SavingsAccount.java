0c50274cf6f68ad4745be830cc70bb19887638e1
memories2013 <nshivaprasad41@gmail.com>
2021-12-22 19:01:49 +0530

    Printing New Balance

--- src/com/entities/SavingsAccount.java
+++ src/com/entities/SavingsAccount.java
@@ -4,20 +4,45 @@ public class SavingsAccount {
 
 	private static double annualInterestRate = 0d;
 	private double savingsBalance;
+	
+	
 
+	public static double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	public static void setAnnualInterestRate(double annualInterestRate) {
+		SavingsAccount.annualInterestRate = annualInterestRate;
+	}
+
+	public double getSavingsBalance() {
+		return savingsBalance;
+	}
+
+	public void setSavingsBalance(double savingsBalance) {
+		this.savingsBalance = savingsBalance;
+	}
+
+	// init savings balance amount
 	public SavingsAccount(double savingsBalance) {
 		super();
 		this.savingsBalance = savingsBalance;
 	}
 
+	// calculate the interest and add to savings
 	public void calculateMonthlyInterest() {
-		savingsBalance *= (annualInterestRate / 100) / 12;
+		double interest = savingsBalance * (annualInterestRate / 100) / 12;
+		savingsBalance += interest;
+		display(interest);
 	}
 	
-	public void display() {
-		System.out.println(" The account has $" + savingsBalance);
+	// display interest and account balance
+	public void display(double interest) {
+		System.out.println(" This account has a monthly interest rate of " + interest);
+		System.out.println(" The new balance of this account is now $" + savingsBalance);
 	}
 	
+	// modify interest rate
 	public static void modifyInterestRate(double newInterestRate) {
 		annualInterestRate = newInterestRate;
 	}
